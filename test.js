842ab8904eec21be2223a92eaa9df2fd448e68c0
Eilish-H <eilish.horsfall@outlook.com>
2021-07-12 17:36:41 +1000

    pushing to organise folder

--- test.js
+++ test.js
@@ -4,8 +4,14 @@
 function replaceWords () {
     const webText = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
     let blockedWord = document.getElementById("unduckMe");
-    
-    for (let i=0; i<Text.length; i++) {
+    let substituteWord = document.getElementById("");
 
+    for (let i=0; i<text.length; i++) 
+    {
+        //for(let j=0; j<blockedWord.length; j++) {}
+        if(text[i].innerHTML.includes(blockedWord)) 
+        {
+            text[i].innerHTML = text[i].innerHTML.replace(blockedWord,`<b>${}</b>`);
+        }
     }
-}
+}   
