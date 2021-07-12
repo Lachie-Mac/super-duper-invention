95e5e46244e9670e0b977170504dcc5d07464965
lmac0025 <lmac0025@student.monash.edu>
2021-07-12 17:32:43 +1000

    class structure

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
 class User 
 {
+    constructor()
+    {
+        this.active = true;
+        this.pin = null;
+        this.bolding = true;
+        
+        this.personaArray = 
+        {
+            trumpActive: true,
+        };
+        
+        this.ruleArray = [];
+
+    }
+    
+    fromData(data)
+    {
+        this.active = active;
+        this.pin = pin;
+        this.bolding = bolding;
+        this.personaArray = personaArray;
+
+        // rule Array recovery
+        for(let i=0;i<data.ruleArray.length;i++)
+        {
+            let newRule = new Rule;
+            newRule.fromData(data.ruleArray[i]);
+            this.ruleArray.push(newRule);
+        }
+    }
+}
+
+class Rule
+{
+    constructor()
+    {
+        this.blockedWords = blockedWords;
+        this.substitute = substitute;
+        this.caseSensitive = caseSensitive;
+        this.blocking = blocking;
+    }
 }
