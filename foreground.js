ddcb083467ede2d9a9d6bd2c5cf33c0882a14594
Dylan Liu <dliu0027@student.monash.edu>
2021-07-15 15:30:46 +1000

    Text replacement algorithm

--- foreground.js
+++ foreground.js
@@ -1,20 +1,39 @@
 
 function updateWords(data,revertActive)
 {
+    let first_duplicate =  []
+    let second_duplicate = []
+    let bolded = true
+
+    for (let i = 0; i < data.length; i++){
+        first_duplicate[i] = {blockWord: data[i].blockWord,
+                    subWord: data[i].subWord}
+    }
+
+    for (let i = 0; i < data.length; i++){
+        second_duplicate[i] = {blockWord: capitalise(data[i].blockWord),
+                    subWord: capitalise(data[i].subWord)}
+    }
+
+    new_duplicate = first_duplicate.concat(second_duplicate)
+
     if(!revertActive)
     {
+        let rules;
         let bigString = document.body.innerHTML;   
-
+        
+        if (bolded){
+            rules = []
+            for (let i = 0; i < new_duplicate.length; i++){
+                rules[i] = {blockWord: `${new_duplicate[i].blockWord}`,
+                              subWord: `<b>${new_duplicate[i].subWord}</b>`}
+              }
+        }
+        else{
+            rules = new_duplicate
+        }
+        
         // RULES - case insensitive, word detection is a bit shet (words must not have letters either side, but dont necessarily need spaces either side)
-        let rules = [{blockWord: "football",
-                subWord: "<b>basketball</b> "},
-                {blockWord: "Football",
-                subWord: "<b>Basketball</b> "},
-                {blockWord: "The",
-                subWord: "<b>THE</b>"},
-                {blockWord: "the",
-                subWord: "<b>THE</b>"}]
-
         for(let i=0;i<rules.length;i++) // rules loop
         {
             for(let j=0;j<bigString.length;j++) // string loop
@@ -22,26 +41,27 @@ function updateWords(data,revertActive)
                 let tester = bigString.substring(j,j+(rules[i].blockWord.length)); 
                 if(rules[i].blockWord==tester)
                 {
+                    if ( !isLetter(bigString.charAt(j-1)) &&  !isLetter(bigString.charAt(j+rules[i].blockWord.length))){
                     for(let k=j+rules[i].blockWord.length;k<bigString.length;k++)
+                {
+                    let testletter = bigString.substring(k,k+1);
+                    if(testletter=="<") // good word
                     {
-                        let testletter = bigString.substring(k,k+1);
-                        if(testletter=="<") // good word
-                        {
-                            let before = bigString.slice(0,j);
-                            let after = bigString.slice(j+rules[i].blockWord.length,bigString.length);
-                            bigString = before + rules[i].subWord + after;
-                            console.log(`Change ${tester}`);
-                            break;
-                        }
-                        else if(bigString.substring(k,k+1)==">")
-                        {
-                            break;
-                        }
+                        let before = bigString.slice(0,j);
+                        let after = bigString.slice(j+rules[i].blockWord.length,bigString.length);
+                        bigString = before + rules[i].subWord + after;
+                        break;
                     }
+                    else if(bigString.substring(k,k+1)==">")
+                    {
+                        break;
+                    }
+                 }
+
+                }
                 }
             }
         }
-        console.log("finished all html")
         document.body.innerHTML = bigString;
     }
 
@@ -49,6 +69,15 @@ function updateWords(data,revertActive)
     {
         
     }
+}
+
+function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i);
+  }
+
+  function capitalise(str){
+    if (typeof(str) !== 'string') {return ''; }
+    return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
 // pull data from storage upon injection
@@ -61,38 +90,6 @@ chrome.storage.local.get('data', data => {
         replaceWords();
     }
 }); 
-
-
-// // function to replace given texts with substitutes
-// function replaceWords(data){
-//     let words = data.dictionary;
-//     let boldingStatus = data.bolding;
-//     let text = document.querySelectorAll("h1, h2, h3, h4, h5, p, b, li, td, caption, span, i , div");
-//     for(let i=0; i<text.length; i++){
-//         let textSegment = text[i].innerHTML;
-//         for(let j=0; j<words.length; j++){
-//             let blockedWord = words[j].blockWord;
-//             let substituteWord = words[j].subWord;
-//             let caseSensitive = words[j].caseSensitive;
-
-//             if(textSegment.toLocaleLowerCase().includes(`${blockedWord}`)){
-//                 // ammend words to allow to check for capital letters
-//                 capitalBlocked = blockedWord.charAt(0).toUpperCase() + blockedWord.slice(1);
-//                 capitalSubstitute = substituteWord.charAt(0).toUpperCase() + substituteWord.slice(1);
-                
-//                 // checking to see if the user wants the replaced words to be bolded
-//                 if(boldingStatus === true){
-//                     substituteWord = `<b>${substituteWord}</b>`;
-//                     capitalSubstitute = `<b>${capitalSubstitute}</b>`;
-//                 }
-
-//                 text[i].innerHTML = text[i].innerHTML.replaceAll(`${blockedWord}`,`${substituteWord}`);
-//                 text[i].innerHTML = text[i].innerHTML.replaceAll(`${capitalBlocked}`,`${capitalSubstitute}`);   
-//             };
-//         };
-//     };
-//     console.log("WORDS REPLACED");
-// };
 
 // listener to trigger reload
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
